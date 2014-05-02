f461940b4d8e02f6f3a3825eeb6147c052b0c188
David Poetzsch-Heffter <davidpoetzsch@web.de>
2014-05-02 15:59:01 +0200

    more work on locales

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,13 @@
 
 DEST=~/.local/share/gnome-shell/extensions/KeyringHelper@sunlight
 
+# compile locales
+for locpath in locale/*; do
+    loc=`basename locpath`
+    msgfmt -o $locpath/LC_MESSAGES/keyman.mo $locpath/LC_MESSAGES/keyman.po
+done
+
 rm -rv $DEST
 mkdir $DEST
-cp -av extension.js metadata.json stylesheet.css $DEST/
+cp -av extension.js keyringDbus.js keyringInterfaces.js metadata.json \
+       COPYING locale/ keyman.pot stylesheet.css $DEST/
