6a3542b68ff7502bcc2adb0f8a318decc065b141
David Poetzsch-Heffter <davidpoetzsch@web.de>
2014-05-07 13:35:18 +0200

    started work on settings

---
+++ settings.js
@@ -0,0 +1,52 @@
+/*
+ * Modified code from text-translator extension
+ */
+
+const Gio = imports.gi.Gio;
+const GLib = imports.gi.GLib;
+const Me = imports.misc.extensionUtils.getCurrentExtension();
+
+const SETTINGS = getSettings();
+
+/**
+ * getSettings:
+ * @schema: (optional): the GSettings schema id
+ *
+ * Builds and return a GSettings schema for @schema, using schema files
+ * in extensionsdir/schemas. If @schema is not provided, it is taken from
+ * metadata['settings-schema'].
+ */
+function getSettings(schema) {
+    schema = schema || Me.metadata['settings-schema'];
+
+    const GioSSS = Gio.SettingsSchemaSource;
+
+    // check if this extension was built with "make zip-file", and thus
+    // has the schema files in a subfolder
+    // otherwise assume that extension has been installed in the
+    // same prefix as gnome-shell (and therefore schemas are available
+    // in the standard folders)
+    let schemaDir = Me.dir.get_child('schemas');
+    let schemaSource;
+
+    if(schemaDir.query_exists(null)) {
+        schemaSource = GioSSS.new_from_directory(
+            schemaDir.get_path(),
+            GioSSS.get_default(),
+            false
+        );
+    }
+    else {
+        schemaSource = GioSSS.get_default();
+    }
+
+    let schemaObj = schemaSource.lookup(schema, true);
+
+    if(!schemaObj)
+        throw new Error(
+            'Schema '+schema+' could not be found for extension '
+            +Me.metadata.uuid+'. Please check your installation.'
+        );
+
+    return new Gio.Settings({ settings_schema: schemaObj });
+}
